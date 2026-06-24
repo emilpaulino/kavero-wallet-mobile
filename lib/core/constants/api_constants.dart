c454a0bba5f1a011bce2c707002ed1161c6e6e39
Emil Paulino <efpl0001@ce.pucmm.edu.do>
2026-06-24 14:56:21 -0400

    chore: improve local API configuration for Android and iOS

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 class ApiConstants {
-
-  static const String baseUrl =
-      'http://localhost:8080';
-
+  static String get baseUrl {
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8080';
+    }
+    return 'http://localhost:8080';
+  }
 }
